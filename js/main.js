ea42689a80157391f0714514dd491b463eb9bd5f
Piotr Pupczyk <piotrek@jakbyco.com>
2015-04-13 22:31:19 +0200

    initial js, project content

---
+++ js/main.js
@@ -0,0 +1,34 @@
+$('.box_portfolio img').click(function() {
+  $(this).after('<div class="project_content"><div class="close"><img src="img/icon.svg#cross" title="Close"></div><div class="content"><div class="project_title"></div><div class="project_description"></div><div class="project_img"></div></div></div>');
+  var clickedProject = $(this).data("project");
+  var projectName = eval(clickedProject).name;
+  var projectDescription = eval(clickedProject).description;
+  var projectUrl = eval(clickedProject).url;
+  $('.project_title').append('<h2>'+ projectName +'</h2>');
+  $('.project_description').append('<span>'+ projectDescription +'</span>');
+  $.each(projectUrl, function(index, value) {
+    $('.project_img').append('<img src="'+ value +'">');
+  });
+
+
+});
+
+$(document).on('click', '.close', function() {
+  $('.project_content').css('display', 'none');
+  $('.project_content').remove();
+});
+
+function Project(name, description, url) {
+  this.name = name;
+  this.description = description;
+  this.url = url;
+}
+
+var figle = new Project('Figle Migle', 
+                        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+                        [ 'img/figle1.jpg' ] );
+           
+var migle = new Project('Migle', 
+                        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+                        [ 'img/figle1.jpg' ] );
+           
